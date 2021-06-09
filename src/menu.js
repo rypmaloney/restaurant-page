60ce62a3af64789639d83075c2057d652dedd383
Ryan Maloney <rypmaloney@gmail.com>
2021-06-09 14:10:27 -0400

    Created menu.js. Initial scripts for menu module.

---
+++ src/menu.js
@@ -0,0 +1,21 @@
+function createMenu() {
+
+
+}
+
+let menuArray[{
+        name: 'Just-A-Sandwich',
+        description: 'Turnkey, bacon, LTO. $7.99',
+        img: sandwich.jpeg,
+    }, {
+        name: 'Bus Burger',
+        description: 'Our famous burger on wheels. two patties, special sauce, LTO. $10.99',
+        img: burger.jpeg,
+    }, {
+        name: 'Speedy Cheese Philly',
+        description: 'Peppers, onions, and our firesauce. $8.99'
+    }, {
+        name: 'Speedy Cheese Philly',
+        description: 'Peppers, onions, and our firesauce. $8.99'
+    }
+]
