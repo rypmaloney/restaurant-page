cea01e0a6ecc3c667eac600b6087a12f7aa8acd4
Ryan Maloney <rypmaloney@gmail.com>
2021-06-09 16:35:45 -0400

    Created inital page load module. Made click events for menu to remove children/load new layout for Menu and About.

---
+++ src/initLoad.js
@@ -0,0 +1,55 @@
+import createAbout from './about.js';
+import createMenu from './menu.js';
+
+console.log('If you see me, everything is A O K');
+
+function createHeader() {
+    const content = document.getElementById('content');
+    const header = document.createElement('div');
+    header.setAttribute('id', 'header');
+    content.appendChild(header);
+
+    const restaurant = document.createElement('h1')
+    restaurant.textContent = 'Bistro Bus';
+    header.appendChild(restaurant);
+
+    const byline = document.createElement('p');
+    byline.textContent = 'A world of food in motion'
+    header.appendChild(byline);
+
+    const nav = document.createElement('nav');
+    header.appendChild(nav);
+
+    const navList = document.createElement('ul');
+    nav.appendChild(navList);
+
+    const aboutBtn = document.createElement('li')
+    aboutBtn.textContent = 'About';
+    aboutBtn.addEventListener('click', () => {
+        removeChildNodes(content);
+        createAbout();
+    });
+
+
+    const menuBtn = document.createElement('li');
+    menuBtn.textContent = 'Menu';
+    menuBtn.addEventListener('click', () => {
+        removeChildNodes(content);
+        createMenu();
+    });
+
+    const contactBtn = document.createElement('li');
+    contactBtn.textContent = 'Contact';
+
+    navList.appendChild(aboutBtn);
+    navList.appendChild(menuBtn);
+    navList.appendChild(contactBtn);
+};
+
+function removeChildNodes(parent) {
+    while (parent.children[1]) {
+        parent.removeChild(parent.children[1]);
+    }
+};
+
+export default createHeader;
